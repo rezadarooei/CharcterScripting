e27b5f182c979fd3d377a3d9bce8bfadd4301181
Daroi\REZA <reza.darooei@gmail.com>
2018-05-20 14:05:26 +0430

    Ch_Receiving Input from the Player

---
+++ Source/CharcterScripting/Private/MyCharacter.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyCharacter.h"
+
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMyCharacter::StartSpring()
+{
+	GLog->Log("StartSprint")
+}
+
+void AMyCharacter::EndSpring()
+{
+	GLog->Log("EndSprint")
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Sprint", this, IE_Pressed, &AMyCharacter::StartSpring);
+	PlayerInputComponent->BindAction("Sprint", this, IE_Released, &AMyCharacter::EndSpring);
+);
+
+}
+
